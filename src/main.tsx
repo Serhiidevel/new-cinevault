4fd1ee1aa1c3eb9de7b72e137229ef63d50e9615
Serhiidevel <serhii.kushnir.u@gmail.com>
2026-05-31 21:01:28 +0200

    fixes

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,12 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
+// Must match Vite `base` in vite.config.ts (GitHub Pages uses /repo-name/)
+const routerBasename = import.meta.env.BASE_URL.replace(/\/$/, "");
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={routerBasename || undefined}>
       <App />
     </BrowserRouter>
   </StrictMode>
